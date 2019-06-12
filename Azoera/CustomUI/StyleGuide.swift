672d3c944270b8b7315ba0b4c51600841c08cdec
Dustin Koch <dustin.koch@me.com>
2019-06-12 09:30:26 -0600

    initial commit with starter project

---
+++ Azoera/CustomUI/StyleGuide.swift
@@ -0,0 +1,22 @@
+//
+//  StyleGuide.swift
+//  TestAzoera
+//
+//  Created by DevMountain on 3/22/19.
+//  Copyright © 2019 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+struct FontNames {
+    static let latoBold = "Lato-Bold"
+    static let latoRegular = "Lato-Regular"
+    static let latoLight = "Lato-Light"
+}
+
+extension UIColor {
+    //TODO: - Add spaceGray and greenAccent Colors
+    static let borderHighlightGray = UIColor(named: "boderHighlight")!
+    static let subltleTextColor = UIColor(named: "subtleText")!
+    static let mainTextColor = UIColor(named: "mainText")!
+    static let blackOverlay = UIColor(named: "blackOverlay")!
+}
